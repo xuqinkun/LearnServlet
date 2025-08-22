f9f2ec9bed4692049b142b8ea69e457bf8938dad
xuqinkun <xu_qinkun@163.com>
2025-08-22 11:39:40 +0800

    2.3 Cookie 表单提交

--- app02a/src/main/java/com/web/servlet/hiddenfields/CustomerServlet.java
+++ app02a/src/main/java/com/web/servlet/hiddenfields/CustomerServlet.java
@@ -1,6 +1,5 @@
 package com.web.servlet.hiddenfields;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
